1c6edc9267e4d11c6cc43c269b31c7910342ba1c
xela <thegamerale97@gmail.com>
2022-04-11 12:03:59 +0200

    commit

--- Form1.vb
+++ Form1.vb
@@ -1,4 +1,5 @@
-﻿Public Class frm_giorniMese
+﻿Imports Microsoft.VisualBasic.Interaction
+Public Class frm_giorniMese
     Private Sub txt_mese_TextChanged(sender As Object, e As EventArgs) Handles txt_mese.TextChanged
 
     End Sub
@@ -13,9 +14,25 @@
     End Sub
 
     Private Sub btn_assegna_Click(sender As Object, e As EventArgs) Handles btn_assegna.Click
-        Dim risposta As Short
+        Dim risposta As String
         Select Case txt_mese.Text
-            Case ""
+            Case "4", "6", "9", "11"
+                lbl_giorni.Text = "numero di giorni: 30"
+            Case "1", "3", "5", "7", "8", "10", "12"
+                lbl_giorni.Text = "numero di giorni: 31"
+            Case "2"
+                risposta = MessageBox.Show("anno bisestile?", "Caso particolare", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+                If risposta = MsgBoxResult.Yes Then
+                    lbl_giorni.Text = "numero di giorni: 29"
+                Else
+                    lbl_giorni.Text = "numero di giorni: 28"
+                End If
+            Case Else
+                lbl_giorni.Text = "numero del mese invalido"
+                txt_mese.Clear()
+                lbl_giorni.Text = ""
+                txt_mese.Focus()
+
         End Select
     End Sub
 End Class
